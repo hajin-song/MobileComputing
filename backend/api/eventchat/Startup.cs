a8741328e41b99a0ee8144ecceb245e6a6f79ae4
Hajin Song <hSong6230@gmail.com>
2017-09-26 17:51:09 +1000

    'commenting'

--- backend/api/eventchat/Startup.cs
+++ backend/api/eventchat/Startup.cs
@@ -5,15 +5,20 @@ using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
 
+// Thie states that the class will run on start
 [assembly: OwinStartup(typeof(eventchat.Startup))]
 namespace eventchat
 {
     public partial class Startup
     {
+        // IAppBuilder provided by the host and is an
+        // interface for composing application for OWIN server
         public void Configuration(IAppBuilder app)
         {
+            // HttpConfiguration used for API routing
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
+            // Connect WebAPI to OWIN server
             app.UseWebApi(config);
         }
     }

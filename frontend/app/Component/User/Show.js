0d737b7d1460b4c369c2d50ed374a84cba6ebfd6
Hajin Song <hSong6230@gmail.com>
2017-10-08 16:57:53 +1100

    Add user activity history

--- frontend/app/Component/User/Show.js
+++ frontend/app/Component/User/Show.js
@@ -20,7 +20,11 @@ import Profile from './Show/Profile';
 const mapStateToProps = (state) => {
  return {
   user: state.User.user,
-  subscriptions: state.User.subscriptions
+  subscriptions: state.User.subscriptions,
+  events: state.Event.events.filter( (event) => {
+   console.log(event);
+   return event.user !== null && event.user.UserName == state.User.user.userName;
+  }),
  }
 };
 
@@ -38,22 +42,17 @@ class UserShow extends Component {
   )
  };
 
- renderList = () => {
+ renderList = (item) => {
   return (
-   <TouchableHighlight onPress={() => this.props.navigation.navigate("PostDetails")}>
-    <View key={item.ID} style={[styles.row]}>
-     <View style={[styles.box]}>
-      <View style={[styles.horizontalBox]} >
-       <Text style={styles.title} > {item.Name} </Text><Text style={styles.date}>{item.Comments} Comment(s)</Text>
-      </View>
-      <Text style={styles.date}>{item.Location}</Text>
-      <Text style={styles.date}>{item.Date}</Text>
+   <View style={[styles.row]}>
+    <View style={[styles.box]}>
+     <View style={[styles.horizontalBox]} >
+      <Text style={styles.title} > {item.Name} </Text><Text style={styles.date}>{item.comments == null ? 0 : item.comments.length} Comment(s)</Text>
      </View>
-     <View style={[styles.box]}>
-      <Image  style={{height:180,marginTop:5,marginBottom:5}} source = {{uri:item.Image}} />
-     </View>
+     <Text style={styles.date}>Coordinate - {item.Latitude}, {item.Longitude}</Text>
+     <Text style={styles.date}>{item.Date}</Text>
     </View>
-   </TouchableHighlight>
+   </View>
   )
  }
 
@@ -74,14 +73,12 @@ class UserShow extends Component {
   return (
    <View style={styles.container}>
     <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0 }}>
-     <FlatList data={this.state.data}
-      renderItem={({item}) => this.renderList}
-      keyExtractor={item => item.ID}
-      onRefresh={this.handleRefresh}
+     <FlatList data={this.props.events}
+      renderItem={({item}) => this.renderList(item)}
+      keyExtractor={item => item.EventID}
       ItemSeparatorComponent={this.renderSeparator}
       ListHeaderComponent={this.renderHeader}
       ListFooterComponent={this.renderFooter}
-      onEndReached={this.handleLoadMore}
       onEndReachedThreshold={50}
       />
     </List>
